16754f17446dd42767a4a35567e03a3c95c76fd0
Neithan Salazar Castaño <128325639+Neithan3568@users.noreply.github.com>
2023-05-08 08:14:16 -0500

    Add files via upload

---
+++ estructura_de-introduccion/app.js
@@ -0,0 +1,32 @@
+
+//Numeros pares o impares 
+let num1 = 7;
+let num2 = 8;
+let num3 = 911;
+let num4 = 100;
+
+if(num1 % 2 == 0) {
+    console.log(num1 + " es par.");
+} else {
+    console.log(num1 + " es impar.");
+}
+
+if(num2 % 2 == 0) {
+    console.log(num2 + " es par.");
+} else {
+    console.log(num2 + " es impar.");
+}
+
+if(num3 % 2 == 0) {
+    console.log(num3 + " es par.");
+} else {
+    console.log(num3 + " es impar.");
+}
+
+if(num4 % 2 == 0) {
+    console.log(num4 + " es par.");
+} else {
+    console.log(num4 + " es impar.");
+}
+
+
